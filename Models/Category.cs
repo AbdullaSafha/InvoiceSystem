0dbe1444dfa1e226bc6910646035c659a04ac54f
Abdulla.Safha <Abdulla.Safha@LAP00620>
2024-07-18 15:58:01 +0530

    Add project files.

---
+++ Models/Category.cs
@@ -0,0 +1,17 @@
+﻿namespace InvoiceSystem.Models
+{
+    public class Category
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string description { get; set; }
+
+        public Category(int _id, string _name, string _description)
+        {
+            id = _id;
+            name = _name;
+            description = _description;
+        }
+
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,28 @@
+﻿namespace InvoiceSystem.Models
+{
+    public class Product
+    {
+        //as name, description, price, quantity, and category.
+        public int id { get; set; }
+        public string name { get; set; }
+        public string description { get; set; }
+        public decimal price { get; set; }
+        public int quantity { get; set; }
+        public Category category { get; set; }
+
+        public Product(int _id, 
+            string _name,
+            string _description,
+            decimal _price,
+            int _quantity,
+            Category _category)
+        {
+            id = _id;
+            price = _price;
+            quantity = _quantity;
+            name = _name;
+            description = _description;
+            category = _category;
+        }
+    }
+}
